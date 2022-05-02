85bcc8ec371038ba9379f3fb78270cdcaa4061ee
Ivan Shtepa <ivanshtepaweb@gmail.com>
2022-05-03 05:44:36 +1000

    Спринт16. перевый коммит

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# yamdb_final
-yamdb_final
+### Описание проекта YaMDb_final:
+
+Вы можете найти любимое произведение в проекте YaMDb. 
+Это может быть книга, фильм или музыкальное произведение. Есть возможность поделиться своим мнением через комментарии,
+читать отзывы и ставить им оценки.
+
+Проект реализован через RESTful API (включая Django и Django REST Framework).
+
+Позволяет делать запросы к моделям проекта: Произведения, Категории, Жанры, Рейтинг, Отзывы.
+
+Поддерживает методы GET, POST, PUT, PATCH, DELETE
+
+Предоставляет данные в формате JSON
+
+### Как запустить проект:
+
+Для начала убедитесь, что у вас установлен Docker командой:
+
+```
+docker -v
+```
+
+Клонируйте репозиторий и перейдите в него в командной строке:
+
+```
+https://github.com/AlinaVoskoboynikova/infra_sp2.git
+cd infra_sp2
+```
+
+Перейдите в папку с проектом и создайте и активируйте виртуальное окружение:
+
+```
+cd api_yamdb
+python3 -m venv env
+```
+
+```
+source venv/Scripts/activate
+```
+
+```
+python3 -m pip install --upgrade pip
+```
+
+Установите зависимости из файла requirements.txt:
+
+```
+pip install -r requirements.txt
+```
+
+Перейдите в папку с файлом docker-compose.yaml:
+
+```
+cd infra
+```
+
+Разверните контейнеры:
+
+```
+docker-compose up -d --build
+```
+
+Выполните миграции, создайте суперпользователя, соберите статику:
+
+```
+docker-compose exec web python manage.py migrate
+docker-compose exec web python manage.py createsuperuser
+docker-compose exec web python manage.py collectstatic --no-input
+```
+
+
+Создайте дамп (резервную копию) базы:
+
+```
+docker-compose exec web python manage.py dumpdata > fixtures.json
+```
+
+
+Примеры запросов:
+
+Пример POST-запроса для аутентифицированного пользователя: добавление нового произведения. POST .../api/v1/titles/
+
+```
+{
+    "name": "string",
+    "year": 0,
+    "description": "string",
+    "genre": [
+        "string"
+    ],
+    "category": "string"
+}
+```
+
+Пример ответа:
+
+```
+{
+    "id": 0,
+    "name": "string",
+    "year": 0,
+    "rating": 0,
+    "description": "string",
+    "genre": [
+       {}
+    ],
+    "category": {
+        "name": "string",
+        "slug": "string"
+    }
+}
+```
+
+Пример POST-запроса для аутентифицированного пользователя: добавление комментария к отзыву. POST .../api/v1/titles/{title_id}/reviews/{review_id}/comments/
+
+```
+{
+    "text": "string"
+}
+```
+
+Пример ответа:
+
+```
+{
+    "id": 0,
+    "text": "string",
+    "author": "string",
+    "pub_date": "2019-08-24T14:15:22Z"
+}
+```
+
+Пример GET-запроса для любого пользователя: получение списка всех отзывов. GET .../api/v1/titles/{title_id}/reviews/
+
+
+Пример ответа:
+
+```
+[
+    {
+    "count": 0,
+    "next": "string",
+    "previous": "string",
+    "results": []
+    }
+]
+```
+
+### Автор проекта yamdb:
+Иван Штепа  
+https://github.com/ivanshtepaweb
---
+++ api_yamdb/api_yamdb/settings.py
@@ -0,0 +1,135 @@
+import os
+from datetime import timedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.getenv('SCRT_KEY', default='123')
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split()
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework_simplejwt',
+    'rest_framework',
+    'django_filters',
+    'api',
+    'reviews',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api_yamdb.urls'
+
+AUTH_USER_MODEL = 'reviews.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'imagination@canneverstop.comherebaby'
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATES_DIR],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'api_yamdb.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='db-name'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres-user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres-password'),
+        'HOST': os.getenv('DB_HOST', default='db-host'),
+        'PORT': os.getenv('DB_PORT', default='1234')
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/hour',
+        'anon': '100/hour',
+    },
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+        'PAGE_SIZE': 10,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+}
+
+TOKEN_OBTAIN_URL = 'http://127.0.0.1:8000/api/v1/auth/token/'
